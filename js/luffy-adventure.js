c17f03a22b771d03371d0aeaff4d36ff19696413
EmanueDevid <emanueldavid150@gmail.com>
2022-11-18 22:05:29 -0300

    criando cenarios

--- js/luffy-adventure.js
+++ js/luffy-adventure.js
@@ -53,8 +53,8 @@ function Carne() {
         this.elemento.style.left = `${x}px`;
     };
 
-    this.setY = (x) => {
-        this.elemento.style.bottom = `${x}px`;
+    this.setY = (y) => {
+        this.elemento.style.bottom = `${y}px`;
     };
 
     this.animarVertical = () => {
@@ -99,6 +99,130 @@ function Luffy() {
         };
     };
 }
+
+function Obstaculo () {
+    this.elemento = novoElemento("div", "obstaculo");
+
+    this.setLargura = (largura) => {
+        this.elemento.style.width = `${largura}px`
+    }
+}
+
+function LinhaDeObstaculos(posicaoNaTela) {
+    this.elemento = novoElemento("div", "linha-obstaculo");
+    const obstaculo1 = new Obstaculo();
+    const obstaculo2 = new Obstaculo();
+    const obstaculo3 = new Obstaculo();
+
+    this.elemento.appendChild(obstaculo1.elemento);
+    this.elemento.appendChild(obstaculo2.elemento);
+    this.elemento.appendChild(obstaculo3.elemento);
+
+    this.getY = () => {
+        return parseInt(this.elemento.style.bottom.split("px")[0]);
+    };
+    
+    this.setY = (posicaoNaTela) => {
+        this.elemento.style.bottom = `${posicaoNaTela}px`;
+    };
+
+    this.setY(posicaoNaTela);
+
+    this.larguraObstaculos = (largura1, largura2, largura3) => {
+        obstaculo1.setLargura(largura1);
+        obstaculo2.setLargura(largura2);
+        obstaculo3.setLargura(largura3);
+    }
+}
+
+function Cenarios(posicaoNaTela) {
+    this.rows = [
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+        new LinhaDeObstaculos(posicaoNaTela),
+    ];
+
+    this.passagemEsquerda = () => {
+        let largura1 = 200;
+        let largura2 = 550;
+        let increment = 50;
+        this.rows.forEach( row => {
+            row.larguraObstaculos(largura1, 0, largura2);
+            row.setY(row.getY() - 2);
+            largura1 += increment;
+            largura2 -= increment;
+
+            if(row.getY() < -600){
+                row.setY(700);
+            }
+        })
+    }
+
+    this.passagemDireita = () => {
+        let largura1 = 550;
+        let largura2 = 200;
+        let increment = 50;
+        this.rows.forEach(row => {
+            row.larguraObstaculos(largura1, 0, largura2);
+            row.setY(row.getY() - 2);
+            largura1 -= increment;
+            largura2 += increment;
+
+            if(row.getY() < -600){
+                row.setY(700);
+            }
+        });
+    }
+
+    this.passagemLivre = () => {
+        this.rows.forEach(row => {
+            row.larguraObstaculos(250, 0, 250);
+            row.setY(row.getY() - 2);
+
+            if(row.getY() < -600){
+                row.setY(700);
+            }
+        })
+    }
+
+    this.bifurcacao = () => {
+        let largura1 = 300;
+        let largura3 = 480;
+        let increment = 40;
+        let newLargura = 50;
+        this.rows.forEach((row, index) => {
+            if(index > 7){
+                row.larguraObstaculos(largura1, 0, largura1);
+                largura1 += increment;
+            }else {
+                row.larguraObstaculos(newLargura, largura3, newLargura);
+                newLargura += 30;
+                largura3 -= 60;
+            }
+
+            row.setY(row.getY() -2);
+
+            if(row.getY() < -600){
+                row.setY(700);
+            }
+        });
+    }
+}
+
+const areaDoJogo = document.querySelector("#game-area");
+const cenario = new Cenarios(600);
+cenario.rows.forEach((row) => {areaDoJogo.appendChild(row.elemento)})
+setInterval(() => {
+    cenario.passagemDireita();
+}, 15);
 
 function estaoSobrepostos(elementoA, elementoB) {
     const a = elementoA.getBoundingClientRect();
